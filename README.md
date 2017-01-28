cf4a548494ae83f633e88ea37c0747b7c62a3149
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-01-28 15:41:57 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
+[![Build Status](https://travis-ci.org/zrrrzzt/russian-kebab.svg?branch=master)](https://travis-ci.org/zrrrzzt/russian-kebab)
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat)](https://github.com/feross/standard)
+
 # russian-kebab
+
 Silly game. One of 6 kebabs is bad. Like really baaaaaaaaad.
+
+## To play
+
+Go [here](https://russiankebab.somuch.party/html) refresh page for another kebab
+
+
+## API
+
+### **/json**
+
+Returns json of result.
+
+#### GET
+
+```bash
+$ curl https://russiankebab.somuch.party/json
+```
+
+### **/html**
+
+Renders result to html. 
+
+#### GET
+
+```bash
+$ curl https://russiankebab.somuch.party/html
+```
+
+
+## License
+[MIT](LICENSE)
+
+![alt text](https://robots.kebabstudios.party/russian-kebab.png "Robohash image of russian-kebab")
---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const readFileSync = require('fs').readFileSync
+const marked = require('marked')
+const { parse } = require('url')
+const { send } = require('micro')
+const renderPage = require('./lib/render-page')
+const getKebab = require('./lib/get-kebab')
+
+module.exports = async (request, response) => {
+  const { pathname } = await parse(request.url, true)
+  if (pathname === '/json' || pathname === '/html') {
+    const results = getKebab()
+
+    if (pathname === '/json') {
+      response.setHeader('Access-Control-Allow-Origin', '*')
+      response.setHeader('Access-Control-Allow-Methods', 'GET')
+      send(response, 200, results)
+    } else if (pathname === '/html') {
+      send(response, 200, renderPage(results))
+    }
+  } else {
+    const readme = readFileSync('./README.md', 'utf-8')
+    const html = marked(readme)
+    send(response, 200, html)
+  }
+}
