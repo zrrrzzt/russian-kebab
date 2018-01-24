237a2cca9951eb7645d9adc4890660ea76344a98
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-01-24 09:14:57 +0100

    Updates Node. Updates test. Adds micro-dev (minor)

--- test/modules/dev-dependencies-test.js
+++ test/modules/dev-dependencies-test.js
@@ -1,10 +1,14 @@
-'use strict'
-
 const tap = require('tap')
 const pkg = require('../../package.json')
 const dependencies = pkg.devDependencies || {}
+const dropModules = ['micro-dev', 'nsp']
+const isDropped = (module) => !dropModules.includes(module)
 
-Object.keys(dependencies).forEach((dependency) => {
-  const module = require(dependency)
-  tap.ok(module, `${dependency} loads ok`)
-})
+if (Object.keys(dependencies).length > 0) {
+  Object.keys(dependencies).filter(isDropped).forEach((dependency) => {
+    const module = require(dependency)
+    tap.ok(module, `${dependency} loads ok`)
+  })
+} else {
+  tap.equal(true, true, 'no dev-dependencies to test')
+}
